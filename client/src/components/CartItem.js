edd40e6036a4647f1088e49411e8658c1a90696d
Zakir Butte <zakir.butte@gmailcom>
2018-03-15 21:41:08 -0700

    added css pages

--- client/src/components/CartItem.js
+++ client/src/components/CartItem.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../css/CartItem.css';
 
 const CartItem = props => {
   return (
--- client/src/components/menuItem.js
+++ client/src/components/menuItem.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import restaurantData from '../restaurantData';
+import '../css/MenuItem.css';
 
 
 const MenuItem = props => {
